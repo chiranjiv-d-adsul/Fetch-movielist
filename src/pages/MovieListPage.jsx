f6a2c0fc7759c247d5857ac05157cc1ba3fd989b
adsul chiru17 <adsulchiranjiv958@gmail.com>
2024-05-30 23:30:07 +0530

    update

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MovieList from '../components/MovieList';
 import FavoritePage from './FavoritesPage';
-import Link from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const MovieListPage = () => {
   return (
